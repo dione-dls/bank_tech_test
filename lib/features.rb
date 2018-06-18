9247a7f06a1ad983cede5943cb5b4002c703b88c
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-18 18:48:20 +0100

    Update features

--- lib/features.rb
+++ lib/features.rb
@@ -1,16 +1,18 @@
 require './lib/account.rb'
 require './lib/transaction_log.rb'
 require './lib/transaction.rb'
+require './lib/printer.rb'
 
 account = Account.new
 transaction_log = TransactionLog.new
+printer = Printer.new(transaction_log)
 transaction_1 = account.deposit(1000)
 transaction_log.add(transaction_1)
 transaction_2 = account.deposit(2000)
 transaction_log.add(transaction_2)
 transaction_3 = account.withdraw(500)
 transaction_log.add(transaction_3)
-transaction_log.all
+printer.statement
 
 # To check if error is raised when amount to be withdrawn is > balance
 # account.withdraw(5000)
