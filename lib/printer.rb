cf7400d7263831a22fd4270d876f4f248a3f550e
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-18 17:27:56 +0100

    Add statement method to Printer

--- lib/printer.rb
+++ lib/printer.rb
@@ -8,4 +8,12 @@ class Printer
   def header
     puts ' date || credit || debit || balance '
   end
+
+  def statement
+    header
+    @all_transactions.all.reverse.each do |transaction|
+    puts " #{ transaction.date } || #{ sprintf('%.2f', transaction.credit_amount) } || #{
+    sprintf('%.2f', transaction.debit_amount) } || #{ sprintf('%.2f', transaction.balance) } "
+    end
+  end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,9 +1,9 @@
 require 'printer'
 
 describe Printer do
-  let(:transaction_1) { double :transaction, balance: 0, amount: 1000, type: 'credit', date: '10/01/2012' }
-  let(:transaction_2) { double :transaction, balance: 1000, amount: 2000, type: 'credit', date: '13/01/2012' }
-  let(:transaction_3) { double :transaction, balance: 3000, amount: 500, type: 'debit', date: '14/01/2012' }
+  let(:transaction_1) { double :transaction, balance: 1000, credit_amount: 1000, debit_amount: 0, date: '10/01/2012' }
+  let(:transaction_2) { double :transaction, balance: 3000, credit_amount: 2000, debit_amount: 0, date: '13/01/2012' }
+  let(:transaction_3) { double :transaction, balance: 2500, credit_amount: 0, debit_amount: 500, date: '14/01/2012' }
   let(:transaction_log) { double :transaction_log, all: [transaction_1, transaction_2, transaction_3] }
   subject(:printer) { described_class.new(transaction_log) }
 
@@ -16,6 +16,17 @@ describe Printer do
   describe '#header' do
     it 'pretty prints a statement header' do
       expect { printer.header }.to output(" date || credit || debit || balance \n").to_stdout
+    end
+  end
+
+  describe '#statement' do
+  let(:pretty_print) { " date || credit || debit || balance \n " +
+    "14/01/2012 || 0.00 || 500.00 || 2500.00 \n " +
+    "13/01/2012 || 2000.00 || 0.00 || 3000.00 \n " +
+    "10/01/2012 || 1000.00 || 0.00 || 1000.00 \n"
+  }
+    it 'pretty prints all the transactions related to the account' do
+      expect { printer.statement }.to output(pretty_print).to_stdout
     end
   end
 end
