b5d8eb21deb8d55bc9d528d4b6315582cb3da2fa
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-19 17:20:47 +0100

    Refactor withdraw and deposit methods

--- lib/account.rb
+++ lib/account.rb
@@ -7,19 +7,17 @@ class Account
 
   def initialize
     @balance = BALANCE
-    @minimum_deposit = MINIMUM_DEPOSIT
-    @minimum_withdrawal = MINIMUM_WITHDRAWAL
   end
 
   def deposit(amount)
-    raise 'Amount below minimum deposit requirement' if below?(@minimum_deposit, amount)
+    raise 'Amount below minimum deposit requirement' if below?(MINIMUM_DEPOSIT, amount)
     @balance += amount
     credit(amount)
   end
 
   def withdraw(amount)
     raise 'Insufficient funds' if insufficient_funds?(amount)
-    raise 'Amount below minimum withdrawal requirement' if below?(@minimum_withdrawal, amount)
+    raise 'Amount below minimum withdrawal requirement' if below?(MINIMUM_WITHDRAWAL, amount)
     @balance -= amount
     debit(amount)
   end
