c242366826c8be7981f0f71219def45341d40939
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-19 12:25:17 +0100

    Revise test descriptions related to transaction

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,19 +4,19 @@ describe Transaction do
   subject(:transaction) { described_class.new(0, 1000, 0) }
 
   describe '#initialize' do
-    it 'returns the balance of the account after a transaction has been made' do
+    it 'has an attribute that returns the balance of the account after a transaction has been made' do
       expect(transaction.balance).to eq 0
     end
 
-    it 'returns the amount of the credit transaction' do
+    it 'has an attribute that returns the amount of the credit transaction' do
       expect(transaction.credit_amount).to eq 1000
     end
 
-    it 'returns the amount of the debit transaction' do
+    it 'has an attribute that returns the amount of the debit transaction' do
       expect(transaction.debit_amount).to eq 0
     end
 
-    it 'returns the transaction date' do
+    it 'has an attribute that returns the transaction date' do
       expect(transaction.date).to eq Date.today.strftime
     end
   end
