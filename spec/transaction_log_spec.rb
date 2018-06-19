1af6e776e5343465d046ad973e8957384893067e
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-19 12:28:16 +0100

    Edit test description for transaction log

--- spec/transaction_log_spec.rb
+++ spec/transaction_log_spec.rb
@@ -13,7 +13,7 @@ describe TransactionLog do
   end
 
   describe '#add' do
-    it 'can record transactions inside the array of all transactions' do
+    it 'records transactions inside the array of all transactions' do
       transaction_log.add(transaction_1)
       transaction_log.add(transaction_2)
       transaction_log.add(transaction_3)
