c789728c01f380009e89701dce37928cadf1b374
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-20 11:53:04 +0100

    Add test for show method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -47,4 +47,19 @@ describe Account do
       account.withdraw(500)
     end
   end
+
+  describe '#show' do
+    let(:pretty_print) {
+      " date || credit || debit || balance \n " \
+    "14/01/2012 || 0.00 || 500.00 || 2500.00 \n " \
+    "13/01/2012 || 2000.00 || 0.00 || 3000.00 \n " \
+    "10/01/2012 || 1000.00 || 0.00 || 1000.00 \n"
+    }
+    it 'prints out the account statement to the screen' do
+      account.deposit(1000, '10/01/2012')
+      account.deposit(2000, '13/01/2012')
+      account.withdraw(500, '14/01/2012')
+      expect { account.statement }.to output(pretty_print).to_stdout
+    end
+  end
 end
