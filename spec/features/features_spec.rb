ebf69a264befb9e6f1bcd41fc79b77b50f3fd703
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-22 11:16:20 +0100

    Refine feature tests

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,15 +4,43 @@ require 'transaction'
 require 'printer'
 
 describe 'Making a deposit' do
-  it 'user makes a deposit by calling the deposit method' do
+  it 'user specifies an amount to deposit' do
     account = Account.new
     account.deposit(1000)
     expect(account.balance).to eq 1000
   end
-
   it 'user makes a deposit below the minimum amount required' do
     account = Account.new
     expect { account.deposit(-100) }.to raise_error RuntimeError
+  end
+end
+
+describe 'Making a withdrawal' do
+  it 'user specifies an amount to withdraw' do
+    account = Account.new
+    account.deposit(1000)
+    account.withdraw(500)
+    expect(account.balance).to eq 500
+  end
+  it 'user makes a withdrawal below the minimum amount required' do
+    account = Account.new
+    account.deposit(1000)
+    expect { account.withdraw(-100) }.to raise_error RuntimeError
+  end
+  it 'user makes a withdrawal more than the current balance in the account' do
+    account = Account.new
+    account.deposit(1000)
+    expect { account.withdraw(5000) }.to raise_error RuntimeError
+  end
+end
+
+describe 'Viewing current balance' do
+  it 'user checks the balance of the account' do
+    account = Account.new
+    account.deposit(1000)
+    account.deposit(2000)
+    account.withdraw(500)
+    expect(account.balance).to eq 2500
   end
 end
 
